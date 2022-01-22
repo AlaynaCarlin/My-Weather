4a153c772f2ce177080265bf8870697465202c06
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-01-21 21:24:51 -0500

    fetch completed, display completed, dtyling probably complete

--- src/components/class.tsx
+++ src/components/class.tsx
@@ -1,38 +1,69 @@
 import React from 'react';
+import DisplayWeather from './function';
 
-class Fetch extends React.Component {
+class Fetch extends React.Component <{},any>{
     constructor(props: any){
         super(props)
         this.state = {
             // set variables for the data from the fetches
             lat: '',
             long: ''
-
         }
     }
 
+    // retrieves location
     fetchLocation(){
-        navigator.geolocation.getCurrentPosition(this.fetchWeather)
+        console.log('location');
+        const baseUrl: string = 'https://api.openweathermap.org/data/2.5/weather';
+        const key: string = '16bfd0882021f8b50da9acac7d8ee70d';
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log('Position:', position)
+            this.setState({
+                lat: position.coords.altitude,
+                long: position.coords.longitude,
+                weatherUrl: `${baseUrl}?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&appid=${key}` 
+            })
+            this.fetchWeather()
+        })
+        
 
     }
 
-    // ? working on it *************************
-    // fetchWeather(position){
-    //     this.setState{
-    //         lat: position.coords.latitude,
-    //         long: position.coords.longitude 
-    //     }
+    fetchWeather=()=>{
+        console.log('weather');
+        // console.log(this.weatherUrl);
+        fetch(this.state.weatherUrl)
+        .then(res => res.json())
+        .then((openWeatherApiResults) => {
+            console.log(openWeatherApiResults);
+            
+            this.setState({
+                temperature: openWeatherApiResults.main.temp,
+                feelsLike: openWeatherApiResults.main.feels_like,
+                humidity: openWeatherApiResults.main.humidity,
+                pressure: openWeatherApiResults.main.pressure,
+                windDeg: openWeatherApiResults.wind.deg,
+                windSpeed: openWeatherApiResults.wind.speed,
+                description: openWeatherApiResults.weather[0].description,
+            })
+        })
 
-    // } 
+    } 
 
-    componentDidMount(){
+    componentDidMount=()=>{
+        console.log('componentDidMount');
         // call the fetches
+        this.fetchLocation()
     }
 
     render() {
+        console.log('render');
         return(
-            <div>
+            <div className='body'>
                 {/* call the function component and send the data through props */}
+                <h1>The current weather in your location</h1>
+                <hr/>
+                <DisplayWeather temp={this.state.temperature} feels={this.state.feelsLike} humidity={this.state.humidity} pressure={this.state.pressure} deg={this.state.windDeg} speed={this.state.windSpeed} desc={this.state.description}/>
             </div>
         )
     }
--- src/components/function.tsx
+++ src/components/function.tsx
@@ -3,7 +3,20 @@ import React from "react";
 const DisplayWeather = (props: any) => {
     return(
         <div>
-
+            <h3>Description: {props.desc}</h3>
+            <hr/>
+            {/* ° = hold alt and type 0176 */}
+            <h3>Temperature: {props.temp}°F</h3>
+            <hr/>
+            <h3>Feels Like: {props.feels}°F</h3>
+            <hr/>
+            <h3>Humidity: {props.humidity}%</h3>
+            <hr/>
+            <h3>Pressure: {props.pressure}PSI</h3>
+            <hr/>
+            <h3>Wind Degrees: {props.deg}</h3>
+            <hr/>
+            <h3>Wind Speed: {props.speed}MPH</h3>
         </div>
     )
 }
